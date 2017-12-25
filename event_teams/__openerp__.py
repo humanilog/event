3d49f074d1d2ff069854a685926a6ab01912e1fe
Stefan Becker <ich@funbaker.de>
2017-12-25 19:09:43 +0100

    add metadata

---
+++ event_teams/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2017 Stefan Becker <s.becker@humanilog.org>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Event Teams",
+    "summary": "Everything you need for managing team events",
+    "version": "9.0.1.0.0",
+    "category": "Event Management",
+    'website': 'http://www.humanilog.org',
+    'author': 'Humanilog, '
+              'Odoo Community Association (OCA)',
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "event",
+    ],
+    "data": [
+    ],
+}
